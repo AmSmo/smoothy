fc94b06e8c85114db4e2b35b8003de327f1f1ccd
AmSmo <adam.smolenski@gmail.com>
2020-08-26 10:59:17 -0400

    Sorry Anson

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,27 +1,29 @@
 class IngredientsController < ApplicationController
     
     def index
-
+        @ingredients = Ingredient.all
     end
 
     def show
-
+        @ingredient = Ingredient.find(params[:id])
     end
 
     def new
-
+        @ingredient = Ingredient.new
     end
 
     def create
-
+        @ingredient = Ingredient.new
+        @ingredient.update!(name: params[:ingredient][:name], smoothy: Smoothy.find_by(name: params[:ingredient][:smoothy])
+        # :smoothy = Smoothy.find_by(name: params[:ingredient][:smoothy])
+        byebug
+        # @ingredient.save
+        # @ingredient.update!(ingredient_params(:name, :smoothy))
     end
 
-    def edit
+    private
 
+    def ingredient_params(*args)
+        params.require(:ingredient).permit(*args)
     end
-
-    def update
-
-    end
-
 end
